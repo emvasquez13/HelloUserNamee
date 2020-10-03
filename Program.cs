3b90eb7dc5537693de9a9c68483f967afdabfb2b
emvasquez13 <70721790+emvasquez13@users.noreply.github.com>
2020-10-03 15:09:15 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Author: Elliot Vasquez
+//This C# console will exercise user input. Go Bulls \m/
+
+using System;
+
+namespace HelloUserName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variable used to store string value
+            string name;
+
+            //Display message
+            Console.WriteLine("Please enter your name");
+
+            //Holds the value that was input by user
+            name = Console.ReadLine();
+
+            //Display Output
+            Console.WriteLine("Welcome {0} GO USF BULLS!", name);
+        }
+    }
+}
